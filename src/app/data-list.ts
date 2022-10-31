4d8bbbebb00a356eadc5ad3e80cadfde18494703
Sachet Acharya <sachetacharya19@gmail.com>
2022-10-31 15:15:23 +0545

    initial

---
+++ src/app/data-list.ts
@@ -0,0 +1,21 @@
+import { data } from './data';
+export const DATA: data[] = [
+  {
+    heading: 'Producy Marketing : Creative Market',
+    name: 'Ram ',
+    views: 458525,
+    description: 'Some quick example text to build on the card title and make ',
+  },
+  {
+    heading: 'Producy Marketing : Creative Market',
+    name: 'Hari',
+    views: 458626,
+    description: 'Some quick example text to build on the card title and make ',
+  },
+  {
+    heading: 'Producy Marketing : Startup Product Sales',
+    name: 'Shyam',
+    views: 465862,
+    description: 'Some quick example text to build on the card title and make ',
+  },
+];
---
+++ src/app/data.ts
@@ -0,0 +1,6 @@
+export interface data{
+    heading?: string
+    name?:string
+    views?:number
+    description?:string
+}
